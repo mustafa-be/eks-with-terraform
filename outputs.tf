b801c551323ff5566a09176766cda416ea062c82
mustafa-be <mustafa899@gmail.com>
2023-03-30 19:34:55 +0500

    Add outputs for EKS Cluster

--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,24 @@ output "ec2_instance_ssh_id" {
 output "ec2_instance_ssh_ip" {
   value = aws_eip.ssh_eip.public_ip
 }
+
+output "eks_cluster_id" {
+  value = aws_eks_cluster.eks_cluster.id
+}
+
+output "eks_cluster_arn" {
+  value = aws_eks_cluster.eks_cluster.arn
+}
+
+output "eks_cluster_certificate_authority_data" {
+  description = "Nested attribute containing certificate-authority-data for your cluster. This is the base64 encoded certificate data required to communicate with your cluster."
+  value       = aws_eks_cluster.eks_cluster.certificate_authority[0].data
+}
+output "eks_cluster_endpoint" {
+  value = aws_eks_cluster.eks_cluster.endpoint
+}
+
+output "eks_cluster_oidc_issuer" {
+  value = aws_eks_cluster.eks_cluster.identity[0].oidc[0].issuer
+}
+
